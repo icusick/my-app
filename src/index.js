2bd1bd373bad07744a755857726c261d143bbf6d
isabel cusick <cusick.isabel@gmail.com>
2017-10-04 00:58:36 -0500

    weather request working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 
@@ -10,7 +11,7 @@ import ParksShow from './components/parks_show.js';
 import reducers from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
